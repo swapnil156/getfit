6b253171e4f23d6ba6f1f18ebff42af47ddbef1d
swapnil156 <swapnillokhande025@gmail.com>
2022-04-04 14:39:58 +0530

    updated

---
+++ plan.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GET FIT</title>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" type="text/css" href="bootstrap.css">
+	<script type="text/javascript" src="jquery.js"></script>
+	<script type="text/javascript" src="popper.js"></script>
+	<script type="text/javascript" src="bootstrap.js"></script>	
+	<link rel="stylesheet" type="text/css" href="Master.css">
+
+	<style type="text/css">
+		h4{
+			color: white;
+		}
+	</style>
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-5 col-sm-5 col-md-6 col-lg-6 col-xl-6">
+					<img src="img/gym.png" class="img-fluid mx-auto" height="70" width="170">
+				</div>
+
+				<div class="col-7 col-sm-7 col-md-6 col-lg-6 col-xl-6">
+					<form class="form-inline" style="margin-top: 35px; margin-left: 60px;">
+						<div class="input-group">
+							<input class="form-control mr-sm-2" type="text" placeholder="Search....">
+							<button class="btn btn-success" type="submit">Submit</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	</header>
+
+	<nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top">
+		<div class="container-fluid">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+	    		<span class="navbar-toggler-icon"></span>
+	  		</button>
+	  		<div class="collapse navbar-collapse" id="collapsibleNavbar">
+				<ul class="navbar-nav mx-auto">
+					<li class="nav-item"><a class="nav-link"  href="home.html" style="width: 110px; text-align: center;">HOME</a></li>
+					<li class="nav-item"><a class="nav-link"  href="sevice.html" style="width: 160px; text-align: center;">SERVICE</a></li>
+					<li class="nav-item"><a class="nav-link"  href="trainers.html" style="width: 160px; text-align: center;">TRAINERS</a></li>
+					<li class="nav-item active" style="border-bottom: 3px solid #77b300; font-size: 17px;"><a class="nav-link"  href="plan.html" style="width: 130px; text-align: center;">PLAN</a></li>
+					<li class="nav-item"><a class="nav-link"  href="Yoga.html" style="width: 110px; text-align: center;">YOGA</a></li>
+					<li class="nav-item"><a class="nav-link"  href="Contact.html" style="width: 110px; text-align: center;">CONTACT</a></li>
+					<li class="nav-item"><a class="nav-link"  href="WeightLoss.html" style="width: 160px; text-align: center;">WEIGHT LOSS</a></li>
+				</ul>
+			</div>	
+		</div>
+	</nav>
+	
+	
+	<section class="plan" id="plan">
+
+<h1 class="heading">membership plan</h1>
+
+<div class="box-container">
+
+    <div class="box">
+        <h3 class="title">basic</h3>
+        <h3 class="price">$10<span>/mo</span></h3>
+        <h3 class="month">3 months</h3>
+        <ul>
+            <li><i class="fas fa-check"></i>weight lifting</li>
+            <li><i class="fas fa-check"></i>cardio</li>
+            <li><i class="fas fa-check"></i>yoga</li>
+            <li><i class="fas fa-check"></i>training</li>
+            <li><i class="fas fa-check"></i>protein powder</li>
+        </ul>
+        <a href="#"><button class="btn">check out</button></a>
+    </div>
+
+    <div class="box">
+        <h3 class="title">standard</h3>
+        <h3 class="price">$20<span>/mo</span></h3>
+        <h3 class="month">6 months</h3>
+        <ul>
+            <li><i class="fas fa-check"></i>weight lifting</li>
+            <li><i class="fas fa-check"></i>cardio</li>
+            <li><i class="fas fa-check"></i>yoga</li>
+            <li><i class="fas fa-check"></i>training</li>
+            <li><i class="fas fa-check"></i>protein powder</li>
+        </ul>
+        <a href="#"><button class="btn">check out</button></a>
+    </div>
+
+    <div class="box">
+        <h3 class="title">premium</h3>
+        <h3 class="price">$30<span>/mo</span></h3>
+        <h3 class="month">12 months</h3>
+        <ul>
+            <li><i class="fas fa-check"></i>weight lifting</li>
+            <li><i class="fas fa-check"></i>cardio</li>
+            <li><i class="fas fa-check"></i>yoga</li>
+            <li><i class="fas fa-check"></i>training</li>
+            <li><i class="fas fa-check"></i>protein powder</li>
+        </ul>
+        <a href="#"><button class="btn">check out</button></a>
+    </div>
+
+</div>
+
+</section>
+
+	
+	
+	
+	
+	
+	
+	
+	
+</body>
+</html>
